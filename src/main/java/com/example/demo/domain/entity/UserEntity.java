d86ffd4d011b73bcda640b6db47e5e8991f904ca
Hepheir <hepheir@gmail.com>
2022-12-29 20:19:04 +0900

    feat: UserEntity에 Getter 추가

--- src/main/java/com/example/demo/domain/entity/UserEntity.java
+++ src/main/java/com/example/demo/domain/entity/UserEntity.java
@@ -1,10 +1,13 @@
 package com.example.demo.domain.entity;
 
+import lombok.Getter;
+
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "user")
+@Getter
 public class UserEntity {
     @Id
     @GeneratedValue
