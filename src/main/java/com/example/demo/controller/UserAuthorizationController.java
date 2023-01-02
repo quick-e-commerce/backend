6b30d25a8fc682b3041c541b8e747b5b2e41b9f0
Hepheir <hepheir@gmail.com>
2023-01-03 08:28:24 +0900

    refactor(controller): UserAuthorizationController애서 UserLoginDTO를 반환하도록 변경
    응답코드 또한 매직 넘버 대신 사전 정의된 HTTP 응답 코드를 사용하도록 변경하였습니다.

--- src/main/java/com/example/demo/controller/UserAuthorizationController.java
+++ src/main/java/com/example/demo/controller/UserAuthorizationController.java
@@ -1,17 +1,21 @@
 package com.example.demo.controller;
 
+import com.example.demo.domain.dto.UserLoginDTO;
 import com.example.demo.exception.UserNotFoundException;
 import com.example.demo.exception.UserPasswordMismatchException;
 import com.example.demo.service.UserAuthorizationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.logging.Logger;
 
 @RestController
+@RequestMapping("/api/v1")
 public class UserAuthorizationController {
     private final Logger logger = Logger.getLogger(UserAuthorizationController.class.getName());
 
@@ -19,22 +23,21 @@ public class UserAuthorizationController {
     UserAuthorizationService userAuthorizationService;
 
     @PostMapping("/auth")
-    public ResponseEntity<String> createAccessToken(
+    public ResponseEntity<UserLoginDTO> createAccessToken(
             @RequestParam String username,
             @RequestParam String password
     ) {
         logger.info("user login request as <" + username + ">.");
         try {
-            String token = userAuthorizationService.createAccessToken(username, password);
-            logger.info("user login as <" + username + ">.");
-            return ResponseEntity.status(201).body(token);
+            UserLoginDTO userLoginDTO = userAuthorizationService.authenticate(username, password);
+            logger.info("user login as <" + userLoginDTO.getUser().getUsername() + ">.");
+            return ResponseEntity.status(HttpStatus.CREATED).body(userLoginDTO);
         } catch (UserNotFoundException e) {
             logger.warning("user failed to login as <" + username + ">.");
-            return ResponseEntity.status(401).body("user not exist");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
         } catch (UserPasswordMismatchException e) {
             logger.warning("user failed to login as <" + username + ">.");
-            return ResponseEntity.status(401).body("password mismatch");
-
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
         }
     }
 }
