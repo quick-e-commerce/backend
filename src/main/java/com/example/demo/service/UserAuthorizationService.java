df6d9613dfa4122a9485d2c865809401ac4436d7
Hepheir <hepheir@gmail.com>
2023-01-03 03:34:43 +0900

    feat(service): UserAuthorizationService 동작 구현

--- src/main/java/com/example/demo/service/UserAuthorizationService.java
+++ src/main/java/com/example/demo/service/UserAuthorizationService.java
@@ -1,6 +1,30 @@
 package com.example.demo.service;
 
+import com.example.demo.domain.entity.UserEntity;
+import com.example.demo.domain.entity.UserEntityRepository;
+import com.example.demo.domain.entity.UserLoginEntity;
+import com.example.demo.domain.entity.UserLoginEntityRepository;
+import com.example.demo.exception.UserNotFoundException;
+import com.example.demo.exception.UserPasswordMismatchException;
+import io.jsonwebtoken.Claims;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.Optional;
+
+@Service
 public class UserAuthorizationService {
+    @Autowired
+    private UserEntityRepository userEntityRepository;
+
+    @Autowired
+    private UserLoginEntityRepository userLoginEntityRepository;
+
+    private final JsonWebTokenService jwtService = new JsonWebTokenService();
+
     /**
      * <p>사용자 인증에 필요한 토큰을 발행합니다.</p>
      * <ul>
@@ -10,10 +34,33 @@ public class UserAuthorizationService {
      * @param username 사용자명
      * @param password 비밀번호
      * @return 새로 생성된 사용자 인증 토큰을 반환합니다.
+     * @throws UserNotFoundException         주어진 이름을 사용하는 사용자가 존재하지 않을 때 발생합니다.
+     * @throws UserPasswordMismatchException 주어진 비밀번호가 해당하는 이름의 사용자 비밀번호와 일치하지 않을 때 발생합니다.
      */
-    public String createAccessToken(String username, String password) {
-        // TODO
-        return null;
+    public String createAccessToken(String username, String password) throws UserNotFoundException, UserPasswordMismatchException {
+        authenticate(username, password);
+        String token = jwtService.create(username);
+        applyToDatabase(token);
+        return token;
+    }
+
+    private void authenticate(String username, String password) throws UserNotFoundException, UserPasswordMismatchException {
+        String actualPassword = ensureFindUserEntity(username).getPassword();
+        if (!actualPassword.equals(password)) {
+            throw new UserPasswordMismatchException();
+        }
+    }
+
+    private void applyToDatabase(String token) throws UserNotFoundException {
+        Claims payload = jwtService.parse(token);
+        Integer userId = ensureFindUserEntity(payload.getAudience()).getId();
+        UserLoginEntity userLoginEntity = userLoginEntityRepository.findById(userId)
+                .orElseGet(() -> UserLoginEntity.builder()
+                        .userId(userId)
+                        .token(token)
+                        .expiresAt(toLocalDateTime(payload.getExpiration()))
+                        .build());
+        userLoginEntityRepository.save(userLoginEntity);
     }
 
     /**
@@ -27,9 +74,24 @@ public class UserAuthorizationService {
      * </ul>
      *
      * @param accessToken 사용자 인증 토큰
+     * @throws UserNotFoundException 주어진 이름을 사용하는 사용자가 존재하지 않을 때 발생합니다.
      */
-    public Void revokeToken(String accessToken) {
-        // TODO
+    public Void revokeToken(String accessToken) throws UserNotFoundException {
+        String username = jwtService.parse(accessToken).getAudience();
+        Integer userId = ensureFindUserEntity(username).getId();
+        userLoginEntityRepository.deleteById(userId);
         return null;
+    }
+
+    private UserEntity ensureFindUserEntity(String username) throws UserNotFoundException {
+        return findUserEntity(username).orElseThrow(UserNotFoundException::new);
+    }
+
+    private Optional<UserEntity> findUserEntity(String username) {
+        return userEntityRepository.findByUsername(username);
+    }
+
+    private LocalDateTime toLocalDateTime(Date date) {
+        return LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault());
     }
 }
