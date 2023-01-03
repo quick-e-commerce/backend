5c7d2c7100f5efef90cb5110f1830dd96342769a
이원규 <85729858+OneK-2@users.noreply.github.com>
2023-01-03 14:53:30 +0900

    feat: ProductEntity 추가

--- src/main/java/com/example/demo/domain/entity/ProductEntity.java
+++ src/main/java/com/example/demo/domain/entity/ProductEntity.java
@@ -1,24 +1,31 @@
 package com.example.demo.domain.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import lombok.Getter;
+
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "product")
+@Getter
 public class ProductEntity {
     @Id
     @GeneratedValue
     private Integer id;
 
-    private Integer shopId;
+    @ManyToOne
+    @JoinColumn(name = "shop_id")
+    private ShopEntity shopEntity;
+    @Column(length = 32)
     private String name;
-    private Integer categoryId;
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private CategoryEntity categoryEntity;
+    @Column(length = 4096)
     private String descriptionHtml;
     private Float fullPrice;
     private Float discountPrice;
+    @Column(length = 128)
     private String thumbnailUrl;
     private LocalDateTime createdAt;
 }
