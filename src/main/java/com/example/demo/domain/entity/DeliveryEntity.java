164ea1172f54d472e3daa96552423e8d959d1f4a
이원규 <85729858+OneK-2@users.noreply.github.com>
2023-01-03 14:39:56 +0900

    feat: DeliveryEntity 추가

---
+++ src/main/java/com/example/demo/domain/entity/DeliveryEntity.java
@@ -0,0 +1,29 @@
+package com.example.demo.domain.entity;
+
+import lombok.Getter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "delivery")
+@Getter
+public class DeliveryEntity {
+    @Id
+    @GeneratedValue
+    private Integer Id;
+
+    @Enumerated(EnumType.STRING)
+    private DeliveryStatus status;
+
+    private Integer cost;
+    @Column(length = 512)
+    private String recipientAddress;
+    @Column(length = 32)
+    private String recipientPhoneNumber;
+    @Column(length = 64)
+    private String recipientEmail;
+    @Column(length = 64)
+    private String recipientFirstName;
+    @Column(length = 64)
+    private String recipientLastName;
+}
