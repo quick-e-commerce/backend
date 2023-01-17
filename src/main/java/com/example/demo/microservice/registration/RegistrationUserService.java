427efff88ff03ab48d219511bb6cdea3747b6112
이원규 <85729858+OneK-2@users.noreply.github.com>
2023-01-17 17:44:25 +0900

    refactor(registration): 공백 사용자명,비밀번호에 대한 검증로직 강화

--- src/main/java/com/example/demo/microservice/registration/RegistrationUserService.java
+++ src/main/java/com/example/demo/microservice/registration/RegistrationUserService.java
@@ -3,6 +3,7 @@ package com.example.demo.microservice.registration;
 import com.example.demo.database.User;
 import com.example.demo.database.UserRepository;
 import com.example.demo.microservice.registration.dto.UserRequestDTO;
+import com.example.demo.microservice.registration.exception.UsernameAlreadyExistException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -13,17 +14,21 @@ public class RegistrationUserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
-    public void create(UserRequestDTO userRequestDTO) throws Exception {
-        validateDuplicateMember(userRequestDTO);
+    public void create(UserRequestDTO userRequestDTO) throws IllegalArgumentException, UsernameAlreadyExistException {
+        validate(userRequestDTO);
         User user = userRequestDTO.toEntity();
         user.setPassword(passwordEncoder.encode(user.getPassword()));
 
         userRepository.save(user);
     }
 
-    private void validateDuplicateMember(UserRequestDTO userDTO) {
+    private void validate(UserRequestDTO userDTO) throws IllegalArgumentException, UsernameAlreadyExistException {
+        if (userDTO.getUsername().isBlank() || userDTO.getPassword().isBlank()) {
+            throw new IllegalArgumentException();
+        }
         if (userRepository.findByUsername(userDTO.getUsername()).isPresent()) {
-            throw new IllegalStateException("이미 존재하는 회원입니다.");
+            throw new UsernameAlreadyExistException();
         }
     }
+
 }
