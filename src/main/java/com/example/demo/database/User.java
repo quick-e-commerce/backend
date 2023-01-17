c7339d47a6cd77fc0068f572c40da0223b7cfaf2
이원규 <85729858+OneK-2@users.noreply.github.com>
2023-01-17 15:52:19 +0900

    refactor(database): lombok Builder 적용

--- src/main/java/com/example/demo/database/User.java
+++ src/main/java/com/example/demo/database/User.java
@@ -1,9 +1,6 @@
 package com.example.demo.database;
 
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -11,25 +8,27 @@ import java.time.LocalDateTime;
 @Entity
 @Table(name = "user")
 @Data
+@Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
     @Id
     @GeneratedValue
     private Integer id;
 
     @Column(length = 16)
+    @NonNull
     private String username;
 
     @Column(length = 64)
+    @NonNull
     private String password;
 
-    @Column(nullable = false)
+    @Column(nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
-    @Builder
-    public User(String username, String password, LocalDateTime createdAt) {
-        this.username = username;
-        this.password = password;
-        this.createdAt = createdAt;
+    @PrePersist
+    private void prePersist() {
+        createdAt = LocalDateTime.now();
     }
 }
